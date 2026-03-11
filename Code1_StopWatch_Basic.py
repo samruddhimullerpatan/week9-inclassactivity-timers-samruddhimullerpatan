66752d6e06633f3a9ad0465be7736b446898d775
samruddhimullerpatan <samruddhi.muller@gmail.com>
2026-03-11 14:03:44 +0530

    Update Code1_StopWatch_Basic.py

--- Code1_StopWatch_Basic.py
+++ Code1_StopWatch_Basic.py
@@ -1,1 +1,33 @@
-#Your Code for making a basic stopwatch using 2 Push Button
+from machine import Pin
+import time
+pb1=Pin(19,Pin.IN,Pin.PULL_UP)
+pb2=Pin(14, Pin.IN, Pin.PULL_UP)
+
+led1=Pin(25,Pin.OUT)
+
+while True:
+    
+    val1=pb1.value()
+    val2=pb2.value()
+    
+    if val1==0:
+        start=time.ticks_ms()
+        print(start)
+        print("start estimating 5 seconds!")
+        led1.on()
+        
+        time.sleep(0.1)
+        
+        
+    if val2==0:
+        end=time.ticks_ms()
+        print(end)
+        led1.off()
+        
+        d=time.ticks_diff(end,start)
+        print(d)
+        
+        esti=5000-d
+        print(esti)
+        
+        time.sleep(0.1)
